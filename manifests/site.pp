5dcea7360fb17f66825b7a65c0d807a8cd480fc0
Shin Jaehun <shinjaehun@linuxmaster.com>
2016-12-22 23:27:38 +0900

    Added vim_upgrade module to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,6 +6,8 @@ node 'vm01.linuxmaster.com' {
 	file {'/tmp/testserver' :
 		content => "This is a test server.\n",
 	}
+	include sshd
+	include vim_upgrade
 }
 
 node /^vm[0-9]+\.linuxmaster\.com$/ {
